344c2f8d340c3f4990a34de631174b4ba7c97a1d
mart-anthony <quasistark21@gmail.com>
2021-10-21 21:04:11 +0800

    1 file modified

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
     <div className="not-found">
       <h1>Ooops!</h1>
       <h3>Page cannot be found</h3>
+      <Link href="/">Go Back to Homepage</Link>
     </div>
   );
 }
